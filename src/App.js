ea83bc5ca2f489c0b9f665a881188341fb2ffaf4
Walter Thumann <wthumann@gmail.com>
2022-11-30 21:09:30 -0300

    desafio resuelto

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import Context from "./context";
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+import Home from "./views/Home";
+import Favoritos from "./views/Favoritos";
+
+// import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
+
+function App() {
+  const endpoint = "/fotos.json";
+  const [photos, setPhotos] = useState([]);
+  const sharedData = { photos, setPhotos };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(endpoint);
+      const data = await res.json();
+      setPhotos(data.photos);
+    };
+    fetchData().catch(console.error);
+  }, []);
+
+  return (
+    <div className="App">
+      <Context.Provider value={sharedData}>
+        <BrowserRouter>
+          <Navbar />
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favoritos" element={<Favoritos />} />
+          </Routes>
+        </BrowserRouter>
+      </Context.Provider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/views/Favoritos.js
@@ -0,0 +1,28 @@
+import { useContext } from "react";
+import Context from "../context";
+
+export default function Favoritos() {
+  const { photos } = useContext(Context);
+
+  return (
+    <div>
+      <h1>Fotos favoritas</h1>
+      <div>
+        {photos &&
+          photos
+            .filter((photo) => photo.liked)
+            .map((photo) => {
+              return (
+                <div>
+                  <img
+                    key={photo.id}
+                    src={photo.src.landscape}
+                    alt={photo.alt}
+                  />
+                </div>
+              );
+            })}
+      </div>
+    </div>
+  );
+}
